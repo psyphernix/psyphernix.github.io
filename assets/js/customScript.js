bbf0eba19aa22896624b86610cad56f8c8c5958a
Bijay Aashish Bhatta <34814966+psyphernix@users.noreply.github.com>
2023-02-16 07:44:46 +0545

    Add files via upload

---
+++ assets/js/customScript.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded",function(){
+    typeSentence("BIJAY AASHISH BHATTA","#feature-text");
+
+    async function typeSentence(sentence, eleRef, delay = 200) {
+        const letters = sentence.split("");
+        let i = 0;
+        while(i < letters.length) {
+          await waitForMs(delay);
+          $(eleRef).append(letters[i]);
+          i++
+        }
+        return;
+      }
+});
+function waitForMs(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms))
+  }
